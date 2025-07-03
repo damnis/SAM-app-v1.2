d97a3be67842a7a3d4ba3ae3656a0778e2fda9bc
damnis <36800988+damnis@users.noreply.github.com>
2025-07-03 22:39:37 +0200

    Update sam_tabel.py

--- sam_tabel.py
+++ sam_tabel.py
@@ -1,4 +1,12 @@
-
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import ta
+import numpy as np
+import plotly.graph_objects as go
+from datetime import datetime, timedelta, date
+import matplotlib.pyplot as plt
+from ta.trend import ADXIndicator
 
 
 
@@ -140,3 +148,26 @@ html += "</tbody></table>"
 
 #st.write("DEBUG signaalkeuze boven Backtest:", signaalkeuze)
 # ---------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# wit
