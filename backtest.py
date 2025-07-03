976061461cb593df5dca151ee30f0a0ac02a2762
damnis <36800988+damnis@users.noreply.github.com>
2025-07-04 01:59:26 +0200

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -165,8 +165,8 @@ def backtest_functie(df, signaalkeuze, selected_tab):
 
         # ➕ Kolomnamen op 2 regels
         df_display = df_display.rename(columns={
-            "SAM-% Koop": "SAM-% Koop",
-            "SAM-% Verkoop": "SAM-% Verkoop goed werkt fit"
+  #          "SAM-% Koop": "SAM-% Koop",
+            "SAM-% Verkoop": "SAM-% Verkoop"
         })
 
         
