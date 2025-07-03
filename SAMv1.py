a7cc0fffb1ca1e1231698a1735693f3af1530ee4
damnis <36800988+damnis@users.noreply.github.com>
2025-07-03 21:41:55 +0200

    Update SAMv1.py

--- SAMv1.py
+++ SAMv1.py
@@ -550,19 +550,6 @@ if toon_koersgrafiek:
     except Exception as e:  
         st.warning(f"Kon y-as limieten niet instellen: {e}")  
 
-    # ➕ y-as: bepaal min/max + marge
-#    try:
-#        koers_values = df_koers["Close"].astype(float).dropna()
-#        if not koers_values.empty:
-#            koers_min = koers_values.min()
- #           koers_max = koers_values.max()
-  #          marge = (koers_max - koers_min) * 0.05
-   #         ax.set_ylim(koers_min - marge, koers_max + marge)
- #       else:
- #           st.warning("Geen geldige koersdata om y-as limieten op te baseren.")
-#    except Exception as e:
-#        st.warning(f"Kon y-as limieten niet instellen: {e}")
-
     # Opmaak
     ax.set_title(f"Koersgrafiek van {ticker_name}")
     ax.set_ylabel("Close")
