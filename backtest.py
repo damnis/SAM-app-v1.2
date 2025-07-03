fc3996e84605bfd0da33da8956080fb0c376f095
damnis <36800988+damnis@users.noreply.github.com>
2025-07-03 22:59:55 +0200

    Create backtest.py

---
+++ backtest.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import ta
+import numpy as np
+import plotly.graph_objects as go
+from datetime import datetime, timedelta, date
+import matplotlib.pyplot as plt
+from ta.trend import ADXIndicator
+
+
