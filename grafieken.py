733fe4bba60b11e5b2554f4d5035a3b089945f69
damnis <36800988+damnis@users.noreply.github.com>
2025-07-03 21:35:56 +0200

    Create grafieken.py

---
+++ grafieken.py
@@ -0,0 +1,12 @@
+# ➕ y-as: bepaal min/max + marge
+#    try:
+#        koers_values = df_koers["Close"].astype(float).dropna()
+#        if not koers_values.empty:
+#            koers_min = koers_values.min()
+ #           koers_max = koers_values.max()
+  #          marge = (koers_max - koers_min) * 0.05
+   #         ax.set_ylim(koers_min - marge, koers_max + marge)
+ #       else:
+ #           st.warning("Geen geldige koersdata om y-as limieten op te baseren.")
+#    except Exception as e:
+#        st.warning(f"Kon y-as limieten niet instellen: {e}")
