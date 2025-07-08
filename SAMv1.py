ef7196afcd3c1a6fefc748293abfe761ec109586
damnis <36800988+damnis@users.noreply.github.com>
2025-07-08 19:36:29 +0200

    Update SAMv1.py

--- SAMv1.py
+++ SAMv1.py
@@ -140,6 +140,7 @@ def determine_advice(df, threshold, risk_aversion=0):
     elif risk_aversion == 1:
         for i in range(2, len(df)):
             sam1 = df["SAM"].iloc[i]
+   #         trends_1 = df["Trend"]
             trend_1 = df["SAT_Trend"].iloc[i]
             trend_2 = df["SAT_Trend"].iloc[i - 1]
             trend_3 = df["SAT_Trend"].iloc[i - 2]
@@ -147,7 +148,7 @@ def determine_advice(df, threshold, risk_aversion=0):
             stage_2 = df["SAT_Stage"].iloc[i - 1]
             stage_3 = df["SAT_Stage"].iloc[i - 2]
 
-            if trend_1 >= trend_2 and trend_2 >= trend_3 and stage_1 > 0:
+            if trend_1 >= trend_2 and trend_2 >= trend_3 and stage_1 > 0 or df["Richting"] == 1) and sam1 >= 0:
                 df.at[df.index[i], "Advies"] = "Kopen"
             elif trend_1 < trend_2 and stage_1 < 0 and stage_2 < 0: # optie and trend_2 < trend_3 
                 df.at[df.index[i], "Advies"] = "Verkopen"
