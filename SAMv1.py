6b1f5426a105aef0dae67a389cf2a180479d8425
damnis <36800988+damnis@users.noreply.github.com>
2025-07-10 21:20:30 +0200

    Update SAMv1.py

--- SAMv1.py
+++ SAMv1.py
@@ -192,7 +192,7 @@ def determine_advice(df, threshold, risk_aversion=0):
             
             if (stage_1 + stage_2 + stage_3 + stage_4 + stage_5) >= 4 and trend_1 >= trend_2 and stage_1 >= 1 and sam_1 >= 2 and (trends_1 - trends_2) >= 0 and trends_1 > 0:
                 df.at[df.index[i], "Advies"] = "Kopen"
-            elif trend_1 < trend_2 and stage_1 < 0:
+            elif sam_1 < 0 and trends_1 < trends_2 or trend_1 < trend_2 and stage_1 < 0:
                 df.at[df.index[i], "Advies"] = "Verkopen"
 
         df["Advies"] = df["Advies"].ffill()
