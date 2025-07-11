aa154f16ca20911bde0b4cecd504a07e39546d59
damnis <36800988+damnis@users.noreply.github.com>
2025-07-11 15:45:19 +0200

    Update grafieken.py

--- grafieken.py
+++ grafieken.py
@@ -15,7 +15,7 @@ def bepaal_grafiekperiode(interval):
     elif interval == "1h":
         return timedelta(days=5)        # 5 dagen à ~7 candles = ±35 punten
     elif interval == "4h":
-        return timedelta(days=60)       # 3 maanden à ~6 candles per week
+        return timedelta(days=30)       # 3 maanden à ~6 candles per week
     elif interval == "1d":
         return timedelta(days=360)      # 180=6 maanden à 1 candle per dag
     elif interval == "1wk":
