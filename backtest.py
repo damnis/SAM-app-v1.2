6e5e3bf94b5c4e45f620d1562e4ad8791291ca68
damnis <36800988+damnis@users.noreply.github.com>
2025-07-11 17:37:14 +0200

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -154,17 +154,19 @@ def backtest_functie(df, signaalkeuze, selected_tab):
         # Definieer kolommen
         geldige_kolommen = ["Markt-%", "SAM-% tot.", "SAM-% Koop", "SAM-% Verkoop"]
 
+        # Bereken het aantal dagen tussen open en sluit datum
+        df_display["Dagen"] = (
+            pd.to_datetime(df_display["Sluit datum"]) - pd.to_datetime(df_display["Open datum"])
+        ).dt.days
+
+        
         # ✅ Afronding en formattering op 2 decimalen met plusteken
         df_display = df_trades.rename(columns={"Rendement (%)": "SAM-% tot."})[[
             "Open datum", "Open prijs", "Sluit datum", "Sluit prijs", "Dagen",
             "Markt-%", "SAM-% tot.", "SAM-% Koop", "SAM-% Verkoop"]]
 
         # Bereken het aantal dagen tussen open en sluit datum
-        # Bereken het aantal dagen tussen open en sluit datum
-        df_display["Dagen"] = (
-            pd.to_datetime(df_display["Sluit datum"]) - pd.to_datetime(df_display["Open datum"])
-        ).dt.days
-
+        
         # ➕ Afronding op 2 decimalen
         for col in ["Markt-%", "SAM-% tot.", "SAM-% Koop", "SAM-% Verkoop"]:
             df_display[col] = df_display[col].astype(float).map("{:+.2f}%".format)
