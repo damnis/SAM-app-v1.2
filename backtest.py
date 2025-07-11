324429cdb0b479e17b4cacc9e2f5c4fdb2aba561
damnis <36800988+damnis@users.noreply.github.com>
2025-07-11 20:27:38 +0200

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import date
+from grafieken import grafiek_periode 
 
 def bereken_sam_rendement(df_signalen, signaal_type="Beide", close_col="Close", df_orig=None):
     rendementen = []
