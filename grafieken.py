656673dd40fac03e9e195c747a65dace691f4101
damnis <36800988+damnis@users.noreply.github.com>
2025-07-13 00:40:55 +0200

    Update grafieken.py

--- grafieken.py
+++ grafieken.py
@@ -239,9 +239,9 @@ def toon_adviesmatrix_html(ticker, risk_aversion=2):
     # ─────────────────────────────────────────────
     # Instellingen per interval
     INTERVALLEN = {
-        "1wk": {"stappen": 3, "breedte": 10, "hoogte": 200, "label": "Week", "show_text": True},
-        "1d": {"stappen": 15, "breedte": 10, "hoogte": 40, "label": "Dag", "show_text": True},
-        "4h": {"stappen": 30, "breedte": 10, "hoogte": 20, "label": "4u", "show_text": True},
+        "1wk": {"stappen": 3, "breedte": 10, "hoogte": 250, "label": "Week", "show_text": True},
+        "1d": {"stappen": 15, "breedte": 10, "hoogte": 50, "label": "Dag", "show_text": True},
+        "4h": {"stappen": 30, "breedte": 10, "hoogte": 22, "label": "4u", "show_text": True},
         "1h": {"stappen": 120, "breedte": 5, "hoogte": 5, "label": "1u", "show_text": True},
         "15m": {"stappen": 480, "breedte": 2, "hoogte": 1, "label": "15m", "show_text": False}
     }
