35436a9ca1c5ccfaa2c51603e8b830eb667a6b87
damnis <36800988+damnis@users.noreply.github.com>
2025-07-13 19:43:02 +0200

    Update genereer.py

--- genereer.py
+++ genereer.py
@@ -29,8 +29,8 @@ def genereer_adviesmatrix(ticker, risk_aversion=2):
 
 
     INTERVALLEN = {
-        "1wk": {"stappen": 3, "breedte": 225, "hoogte": 20, "label": "Week", "show_text": True},
-        "1d": {"stappen": 15, "breedte": 44.6, "hoogte": 20, "label": "Dag", "show_text": True},
+        "1wk": {"stappen": 3, "breedte": 222, "hoogte": 20, "label": "Week", "show_text": True},
+        "1d": {"stappen": 15, "breedte": 44.58, "hoogte": 20, "label": "Dag", "show_text": True},
         "4h": {"stappen": 45, "breedte": 14.76, "hoogte": 10, "label": "4u", "show_text": True},
         "1h": {"stappen": 135, "breedte": 4.825, "hoogte": 10, "label": "1u", "show_text": True},
         "15m": {"stappen": 540, "breedte": 1.1, "hoogte": 6, "label": "15m", "show_text": False}
