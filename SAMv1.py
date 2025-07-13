5ae7584ef31d41896b6bfe48fa0f799ef5deef46
damnis <36800988+damnis@users.noreply.github.com>
2025-07-13 13:16:46 +0200

    Update SAMv1.py

--- SAMv1.py
+++ SAMv1.py
@@ -22,6 +22,7 @@ from sat_indicator import calculate_sat
 from adviezen import determine_advice, weighted_moving_average 
 # grafieken en tabellen
 from grafieken import plot_koersgrafiek, plot_sam_trend, plot_sat_debug, bepaal_grafiekperiode, plot_overlay_grafiek 
+from genereer import genereer_adviesmatrix 
 from grafieken import toon_adviesmatrix_html
 from sam_tabel import toon_sam_tabel 
 # Backtestfunctie 
