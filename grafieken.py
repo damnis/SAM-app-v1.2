d810fcbdcd732eff2a1aadbf89356b75be89c15f
damnis <36800988+damnis@users.noreply.github.com>
2025-07-13 20:33:02 +0200

    Update grafieken.py

--- grafieken.py
+++ grafieken.py
@@ -21,11 +21,11 @@ from streamlit.components.v1 import html as st_html
 # 📆 Periode voor SAM-grafiek op basis van interval
 def bepaal_grafiekperiode(interval):
     if interval == "15m":
-        return timedelta(days=10)        # 7 dagen à ~96 candles per dag = ±672 punten
+        return timedelta(days=7)        # 7 dagen à ~96 candles per dag = ±672 punten
     elif interval == "1h":
         return timedelta(days=15)        # 5 dagen à ~7 candles = ±35 punten
     elif interval == "4h":
-        return timedelta(days=90)       # 3 maanden à ~6 candles per week
+        return timedelta(days=75)       # 3 maanden à ~6 candles per week
     elif interval == "1d":
         return timedelta(days=120)      # 180=6 maanden à 1 candle per dag
     elif interval == "1wk":
