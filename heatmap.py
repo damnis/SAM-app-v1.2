a811fc8cb99a07859f8942069d2011a5723516d1
damnis <36800988+damnis@users.noreply.github.com>
2025-07-14 08:42:19 +0200

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -21,7 +21,7 @@ def genereer_sector_heatmap(interval, risk_aversion=2):
 
         for ticker in tickers[:20]:
             try:
-                df = fetch_data_cached(ticker, interval=interval, period="6mo")
+                df = fetch_data_cached(ticker, interval=interval, period="2mo")
                 if df is None or df.empty or len(df) < 50:
                     raise ValueError("Onvoldoende data")
 
