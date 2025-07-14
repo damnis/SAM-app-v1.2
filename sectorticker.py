abca396e12860cf02fcc909f566b3d9b9cad5538
damnis <36800988+damnis@users.noreply.github.com>
2025-07-14 13:10:18 +0200

    Update sectorticker.py

--- sectorticker.py
+++ sectorticker.py
@@ -2,7 +2,7 @@
 
 # Elke sector bevat een lijst van 16 tickers (of minder bij opstarten)
 sector_tickers = {
-    "Tech": ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "ADBE", "CRM", "INTC", "CSCO", "AMD", "ORCL", "AVGO", "TXN", "QCOM", "IBM"],
+    "Tech": ["AAPL", "PLTR", "SMCI", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "ADBE", "CRM", "INTC", "CSCO", "UBER", "TSLA", "AMD", "ORCL", "AVGO", "TXN", "QCOM", "IBM"],
 #    "Financials": ["JPM", "BAC", "WFC", "C", "GS", "MS", "AXP", "BK", "USB", "SCHW", "TFC", "COF", "MTB", "FITB", "HBAN", "PNC"],
 #    "Energy": ["XOM", "CVX", "COP", "EOG", "PSX", "VLO", "MPC", "HES", "OKE", "PXD", "WMB", "KMI", "SLB", "BKR", "HAL", "APA"],
 #    "Crypto": ["BTC-USD", "ETH-USD", "SOL-USD", "BNB-USD", "XRP-USD", "ADA-USD", "DOGE-USD", "DOT-USD", "AVAX-USD", "LINK-USD", "MATIC-USD", "LTC-USD", "BCH-USD", "XLM-USD", "ATOM-USD", "UNI-USD"],
