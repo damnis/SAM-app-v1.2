5ec1813dbe68af9121dd3358915323a8b2180538
damnis <36800988+damnis@users.noreply.github.com>
2025-07-14 16:14:06 +0200

    Update SAMv1.py

--- SAMv1.py
+++ SAMv1.py
@@ -321,7 +321,7 @@ with col2:
 # Bijvoorbeeld ergens onder je toggle:
 if st.toggle("Toon sector heatmap"):
     sortering = st.radio("Sorteer tickers per sector op:", ["marktkapitalisatie", "alfabetisch"], horizontal=True)
-    toon_sector_heatmap(interval, risk_aversion, sortering)
+    toon_sector_heatmap(interval, risk_aversion)
 #    toon_sector_heatmap(sortering=marktkapitalisatie)
 #toon_heatmap = st.toggle("📊 Toon sector heatmap", value=False)
 
