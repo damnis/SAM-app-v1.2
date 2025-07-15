a6d06aae07f209ab10aac59e1c22a8d3ebaa4551
damnis <36800988+damnis@users.noreply.github.com>
2025-07-15 23:35:49 +0200

    Update sectorticker.py

--- sectorticker.py
+++ sectorticker.py
@@ -5,13 +5,13 @@ sector_tickers = {
     "US Tech": ["AAPL", "PLTR", "SMCI", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "ADBE", "CRM", "INTC", "CSCO", "UBER", "TSLA", "AMD", "ORCL", "AVGO", "TXN", "QCOM", "IBM"],
 #    "Financials": ["JPM", "BAC", "WFC", "C", "GS", "MS", "AXP", "BK", "USB", "SCHW", "TFC", "COF", "MTB", "FITB", "HBAN", "PNC"],
 #    "Energy": ["XOM", "CVX", "COP", "EOG", "PSX", "VLO", "MPC", "HES", "OKE", "PXD", "WMB", "KMI", "SLB", "BKR", "HAL", "APA"],
-    "Crypto": ["BTC-USD", "ETH-USD", "SOL-USD", "BNB-USD", "XRP-USD", "ADA-USD", "DOGE-USD", "DOT-USD", "AVAX-USD", "LINK-USD", "MATIC-USD", "LTC-USD", "BCH-USD", "XLM-USD", "ATOM-USD", "UNI-USD"],
+    "Crypto": ["BTC-USD", "ETH-USD", "SOL-USD", "BNB-USD", "XRP-USD", "ADA-USD", "DOGE-USD", "LINK-USD", "LTC-USD", "XLM-USD"],
     "Nederland": ["ABN.AS", "ADYEN.AS", "AGN.AS", "AD.AS", "AKZA.AS", "MT.AS", "ASM.AS", "ASML.AS", "ASRNL.AS", "BESI.AS", "DSFIR.AS", "HEIA.AS", "IMCD.AS", "INGA.AS", "TKWY.AS", "KPN.AS", "NN.AS", "PHIA.AS", "UNA.AS", "WKL.AS"]
     
     
     # Voeg eenvoudig meer sectoren toe in hetzelfde formaat
 }
-
+#DOT-USD", "AVAX-USD", ""MATIC-USD""BCH-USD""ATOM-USD", "UNI-USD"
 
 
 
