3cdfce2130ebb83a68ebce16f8a5748ee26aca77
damnis <36800988+damnis@users.noreply.github.com>
2025-07-15 19:07:38 +0200

    Update SAMv1.py

--- SAMv1.py
+++ SAMv1.py
@@ -50,6 +50,8 @@ from alpaca.data.timeframe import TimeFrame
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
+from alpaca.trading.requests import OptionOrderRequest
+
 #import alpaca_trade_api as tradeapi
 from fmpfetch import fetch_data_fmp, search_ticker_fmp
 
