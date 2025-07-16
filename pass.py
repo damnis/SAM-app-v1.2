2d09892d70d0c11c3aefe242855face17d481da1
damnis <36800988+damnis@users.noreply.github.com>
2025-07-16 08:19:18 +0200

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+def password_gate():
+    PASSWORD = st.secrets["APP_PASSWORD"]
+    if "pw_correct" not in st.session_state:
+        st.session_state.pw_correct = False
+
+    if not st.session_state.pw_correct:
+        pw = st.text_input("Wachtwoord", type="password")
+        if pw == PASSWORD:
+            st.session_state.pw_correct = True
+            st.experimental_rerun()
+        elif pw:
+            st.error("Wachtwoord onjuist!")
+        st.stop()  # stopt de hele app als niet correct!
+
+
+
+
+
+
+
+
+
+
+
