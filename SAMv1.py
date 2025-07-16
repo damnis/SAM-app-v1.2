1e3c556e73e262ee7efa87948669c1f55ef9fe9e
damnis <36800988+damnis@users.noreply.github.com>
2025-07-16 08:20:24 +0200

    Update SAMv1.py

--- SAMv1.py
+++ SAMv1.py
@@ -54,7 +54,9 @@ from alpaca.trading.enums import OrderSide, TimeInForce
 
 #import alpaca_trade_api as tradeapi
 from fmpfetch import fetch_data_fmp, search_ticker_fmp
-
+# acces control
+from pass import password_gate
+password_gate()
 
  
 # --- Streamlit UI ---
