b362be9bb18ba92935e7ca65b3dba68b8d8ba3e1
damnis <36800988+damnis@users.noreply.github.com>
2025-07-20 19:02:30 +0200

    Update SAMv1.py

--- SAMv1.py
+++ SAMv1.py
@@ -371,7 +371,7 @@ else:
 
 # screening tool
 if st.button("🔎 Zoek koopwaardige aandelen"):
-    screeneresult = screen_tickers(tickers_screening, min_marketcap=1e6, min_momentum=5)
+    screeneresult = screen_tickers(tickers_screening, min_momentum=5)
     st.dataframe(screeneresult)
 
 
