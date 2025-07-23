d55100509a46a31eda81229ca401fdd2fac02745
damnis <36800988+damnis@users.noreply.github.com>
2025-07-23 18:44:01 +0200

    Update fmpfetch.py

--- fmpfetch.py
+++ fmpfetch.py
@@ -94,6 +94,7 @@ def fetch_data_fmp(ticker, periode="10y"):
         df["Open"] = df["Close"]
         df["High"] = df["Close"]
         df["Low"] = df["Close"]
+        df["Volume"] = df["Volume"]
 
         # 📉 Indicatoren
         df["MA35"] = df["Close"].rolling(window=35, min_periods=1).mean()
